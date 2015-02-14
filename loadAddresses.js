de514544e26f429b89c6c61022f6f56b540bfecc
Hoang Nguyen <hvn1@hvn1-Inspiron-N5040.(none)>
2015-02-14 17:35:26 -0500

    Moved json addr to new dir

--- loadAddresses.js
+++ loadAddresses.js
@@ -1,4 +1,7 @@
 //build a namespace for this
-var Ptolemy = Ptolemy || {};
+var ptolemy = ptolemy || {};
 
+//load the addresses into javascript memory
+ptolemy.buildingAddrUri = "json_addr/buildings.json";
+ptolemy.lotAddrPathUri = "https://raw.githubusercontent.com/thehexia/Ptolemy/master/parkinglots.json";
 
